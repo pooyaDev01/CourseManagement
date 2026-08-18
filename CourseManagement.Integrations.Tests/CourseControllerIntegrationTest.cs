4041098e5172e1be1f7d5ccad371e98162af3f69
Pooya <pooya.dev01@gmail.com>
2026-08-18 11:19:08 +0330

    feat: create course controller integration test

---
+++ CourseManagement.Integrations.Tests/CourseControllerIntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace CourseManagement.Integrations.Tests;
+public class CourseControllerIntegrationTest
+{
+    [Fact]
+    public async Task GetAll_Should_ReturnOk_WhenCoursesExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
+
