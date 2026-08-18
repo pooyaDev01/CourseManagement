e4b70b2da4cfb06e92ff04d2715f0273aefbd070
Pooya <pooya.dev01@gmail.com>
2026-08-19 00:03:26 +0330

    test: complete the lesson integration test

--- CourseManagement.Application/DTOs/Lesson/UpdateLessonRequestDto.cs
+++ CourseManagement.Application/DTOs/Lesson/UpdateLessonRequestDto.cs
@@ -9,7 +9,6 @@ public class UpdateLessonRequestDto
     [Required]
     [MaxLength(200)]
     public string Title { get; set; } = string.Empty;
-    [MaxLength(1000)]
 
     [Range(1, 600)]
     public int DurationInMinutes { get; set; }
