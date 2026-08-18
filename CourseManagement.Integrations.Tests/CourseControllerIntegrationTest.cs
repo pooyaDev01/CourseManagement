1a84acd98feb71a5ee6573a9b87c0962de1d0106
Pooya <pooya.dev01@gmail.com>
2026-08-18 12:07:32 +0330

    feat: create an instance of custom web application factory and create http client through that to access the client http methods.

--- CourseManagement.Integrations.Tests/CourseControllerIntegrationTest.cs
+++ CourseManagement.Integrations.Tests/CourseControllerIntegrationTest.cs
@@ -4,8 +4,14 @@ using System.Text;
 using System.Net.Http;
 
 namespace CourseManagement.Integrations.Tests;
-public class CourseControllerIntegrationTest
+public class CourseControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
 {
+    private readonly HttpClient _client;
+
+    public CourseControllerIntegrationTest(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
     [Fact]
     public async Task GetAll_Should_ReturnOk_WhenCoursesExists()
     {
