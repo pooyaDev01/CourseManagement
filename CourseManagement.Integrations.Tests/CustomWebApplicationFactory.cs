24f730046532c337096f83922a3b708af066282f
Pooya <pooya.dev01@gmail.com>
2026-08-18 11:57:10 +0330

    feat: setting up the entity framwork in memory, to create new and empty database for each integration tests we run

--- CourseManagement.Integrations.Tests/CustomWebApplicationFactory.cs
+++ CourseManagement.Integrations.Tests/CustomWebApplicationFactory.cs
@@ -1,9 +1,37 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using CourseManagement.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CourseManagement.Integrations.Tests;
-public class CustomWebApplicationFactory
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.UseEnvironment("Test");
+
+        builder.ConfigureServices(services =>
+        {
+            var descripter = services.SingleOrDefault(temp => temp.ServiceType ==
+            typeof(DbContextOptions<AppDbContext>));
+
+            if (descripter != null)
+            {
+                services.Remove(descripter);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("DatabaseForTesting"); //it means this ef-core in memory, everytime executes for testing, it will regenerate the new and empty databse for integration testing
+            });
+        });
+    }
 }
 
