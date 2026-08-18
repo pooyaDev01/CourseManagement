a6a77b4285264d5480eb38a06f23eeb35bf3a124
Pooya <pooya.dev01@gmail.com>
2026-08-18 18:41:03 +0330

    feat: add new integration tests for course

--- CourseManagement.Integrations.Tests/CourseControllerIntegrationTest.cs
+++ CourseManagement.Integrations.Tests/CourseControllerIntegrationTest.cs
@@ -8,6 +8,7 @@ using Xunit.Abstractions;
 using System.Net;
 using CourseManagement.Application.DTOs.Course;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseManagement.Integrations.Tests;
 public class CourseControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
@@ -119,6 +120,138 @@ public class CourseControllerIntegrationTest : IClassFixture<CustomWebApplicatio
         // Assert
         responseMessage1.StatusCode.Should().Be(HttpStatusCode.Created);
         responseMessage2.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnOk_WhenCourseExists()
+    {
+        // Arrange - Reset database for fresh test
+        await _factory.ResetDatabaseAsync();
+
+        
+        var request = new AddCourseRequestDto
+        {
+            Title = "course1",
+            Description = "course1 description",
+            Price = 100
+        };
+
+        var createdCourse = await _client.PostAsJsonAsync("api/course", request);
+
+        var contentOfCreatedCourse = await createdCourse.Content.ReadFromJsonAsync<CourseResponseDto>();
+        var contentOfCreatedCourse2 = await createdCourse.Content.ReadAsStringAsync();
+
+        // Log
+        _output.WriteLine($"Craeted Content: {contentOfCreatedCourse2}");
+
+        // Act
+        var getCourse = await _client.GetAsync($"api/course/{contentOfCreatedCourse!.Id}");
+
+        // Log
+        _output.WriteLine("Status Code: " + getCourse.StatusCode);
+        _output.WriteLine("Status Code Number: " + (int)getCourse.StatusCode);
+        _output.WriteLine("Get Content: " + await getCourse.Content.ReadAsStringAsync());
+        _output.WriteLine("Is Success: " + getCourse.IsSuccessStatusCode);
+
+        // Assert
+        getCourse.IsSuccessStatusCode.Should().BeTrue();
+        getCourse.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetById_Should_ReturnNotFound_WhenCourseDoesNotExist()
+    {
+        // Arrange - Reset database for fresh test
+        await _factory.ResetDatabaseAsync();
+
+        int id = 1;
+
+        // Act
+        var responseMessage = await _client.GetAsync($"api/course/{id}");
+
+        // Log
+        _output.WriteLine("Status Code: " + responseMessage.StatusCode);
+        _output.WriteLine("Status Code: " + (int)responseMessage.StatusCode);
+        _output.WriteLine("Is Success: " + responseMessage.IsSuccessStatusCode);
+
+        // Assert
+        responseMessage.IsSuccessStatusCode.Should().BeFalse();
+        responseMessage.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Update_ShouldUpdateSuccessfully_WhenCourseExists()
+    {
+        // Arrange - Reset database for fresh test
+        await _factory.ResetDatabaseAsync();
+
+        var createRequest = new AddCourseRequestDto
+        {
+            Title = "course 1",
+            Description = "course 1 description",
+            Price = 100
+        };
+
+        var createResponse = await _client.PostAsJsonAsync("api/course", createRequest);
+
+        var createJsonResponse = await createResponse.Content.ReadFromJsonAsync<CourseResponseDto>();
+
+        var id = createJsonResponse!.Id;
+
+        var Createcontent = await createResponse.Content.ReadAsStringAsync();
+
+        // Log
+        _output.WriteLine("Created Content: " + Createcontent);
+
+        var updaterequest = new UpdateCourseRequestDto
+        {
+            Title = "Modified Title",
+            Description = "Modified Description",
+            Price = 200
+        };
+
+        // Act
+        var updateResponse = await _client.PutAsJsonAsync($"api/course/{id}", updaterequest);
+
+        var getAll = await _client.GetAsync("api/course");
+
+        // Log
+        _output.WriteLine("Status Code: " + updateResponse.StatusCode);
+        _output.WriteLine("Status Code: " + (int)updateResponse.StatusCode);
+        _output.WriteLine("Update Content: " + await getAll.Content.ReadAsStringAsync()); // if you log the update since it's response is no-content ,so it's log will be empty
+        _output.WriteLine("Is Success: " + updateResponse.IsSuccessStatusCode);
+
+        // Assert
+        updateResponse.IsSuccessStatusCode.Should().BeTrue();
+        updateResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Update_ShouldReturnNotFound_WhenCourseDoesNotExist()
+    {
+        // Arrange - Reset database for fresh test
+        await _factory.ResetDatabaseAsync();
+
+        int id = 1;
+
+        var request = new UpdateCourseRequestDto
+        {
+            Title = "Modified Title",
+            Description = "Modified Description",
+            Price = 200
+        };
+
+        // Act
+        var responseMessage = await _client.PutAsJsonAsync($"api/course/{id}", request);
+
+        // Log
+        _output.WriteLine("Status Code: " + responseMessage.StatusCode);
+        _output.WriteLine("Status Code: " + (int)responseMessage.StatusCode);
+        _output.WriteLine("Is Success: " + responseMessage.IsSuccessStatusCode);
+
+        // Assert
+        responseMessage.IsSuccessStatusCode.Should().BeFalse();
+        responseMessage.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
 
